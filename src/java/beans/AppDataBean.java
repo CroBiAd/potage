cd04aa577ce3703d2b3f9d1a88fc1e8e3ba0036a
Radoslaw Suchecki <radoslaw.suchecki@acpfg.com.au>
2017-01-13 12:18:54 +1030

    bean serializable

--- src/java/beans/AppDataBean.java
+++ src/java/beans/AppDataBean.java
@@ -22,6 +22,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -46,7 +47,7 @@ import reusable.Reporter;
  */
 @ManagedBean(name = "appDataBean")
 @ApplicationScoped
-public final class AppDataBean {
+public final class AppDataBean implements Serializable {
 
     private boolean DEBUG;
     private final String DEBUG_PREFIX = "Traes_1A";
